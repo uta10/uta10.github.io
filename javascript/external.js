bf6e095de8011d9c72a83067da61911f4197ab43
Courtney Holifield <uta10@txstate.edu>
2026-09-16 13:39:49 -0500

    Replaced the header with user prompt information.

--- javascript/external.js
+++ javascript/external.js
@@ -1,8 +1,4 @@
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-document.writeln(arr[i],"<br>");
-}
+
 // A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
