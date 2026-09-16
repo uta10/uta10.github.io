8591b004a7b0f57ca9f27d9e2a51bb475abd4502
Courtney Holifield <uta10@txstate.edu>
2026-09-16 13:05:10 -0500

    Completed Task 5 Conditional Statement.

--- javascript/external.js
+++ javascript/external.js
@@ -1,36 +1,13 @@
-// document.write("Where is my JS code?");
-document.write("Where is my JS code?");
-// window.alert(5 + 6);
-// window.alert('5 + 6');
-window.alert(5 + 6);
-window.alert('5 + 6');
-// document.write('<h1>A heading</h1>');
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
-// const x = "web";
-const x = "web";
-// const y = "mapping";
-const y = "mapping";
-// const out = x + y;
-const out = x + y;
-// document.write(out);
-document.write(out);
-// A prompt box is used to prompt users to input a value before entering a page.
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(user_name);
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
-// a = 22;
-// const b = 33;
-// document.writeln(a + b);
-// document.writeln("<br>");
-// document.writeln(a += b);
-// document.writeln("<br>");
-// document.writeln(a += b);
-a = 22;
-const b = 33;
-document.writeln(a + b);
-document.writeln("<br>");
-document.writeln(a += b);
-document.writeln("<br>");
-document.writeln(a += b);
+document.writeln("<button onclick='condition()'>Conditional Test</button>");
+function condition()
+{
+x = confirm("Are you sure you want to proceed?");
+if(x)
+{
+document.writeln("You chose Okay!");
+}
+else
+{
+document.writeln("You chose Cancel!");
+}
+}
