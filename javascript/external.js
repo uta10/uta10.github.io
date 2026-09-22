f79ecb59ceb8495a6a6e7f71adacae88acdb135e
Courtney Holifield <uta10@txstate.edu>
2026-09-22 16:18:09 -0500

    Automated table creation for webmap reviews.

--- javascript/external.js
+++ javascript/external.js
@@ -11,3 +11,18 @@ user_name = window.prompt(a, b);
 message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
 return message
 }
+function webmap_table()
+{
+  document.write("<table width=100%>");
+for (var row=0; row < 2; row++)
+{
+document.write("<tr>");
+for (var column=0; column < 3; column++)
+{
+document.write("<td>" + row + "," + column + "</td>");
+}
+document.write("</tr>");
+}
+document.write("</table>");
+return "";
+}
